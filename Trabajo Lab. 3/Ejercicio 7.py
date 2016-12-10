95687ab2613e40e2a72a76b938c354e0970cb380
alejandronava13 <alejandro13nava7@gmail.com>
2016-12-09 23:41:54 -0600

    Update Ejercicio 7.py
    Canción Semi-optimizada

--- Trabajo Lab. 3/Ejercicio 7.py
+++ Trabajo Lab. 3/Ejercicio 7.py
@@ -4,13 +4,10 @@ n=raw_input('Ingrese nombre' ).title()
 s=map(str,n)
 v=('a','e','i','o','u','A','E','I','O','U')
 if n[0]==v[0] or n[0]==v[1] or n[0]==v[2] or n[0]==v[3] or n[0]==v[4]:
-    print "La palabra empieza por vocal"
     new=n.lower()
 elif n[0]==v[5] or n[0]==v[6] or n[0]==v[7] or n[0]==v[8] or n[0]==v[9]:
-    print "La palabra empieza por vocal"
     new=n.lower()
 else:
-    print "La palabra no empieza por vocal"
     new=n[1:]
     if n[1]==v[0] or n[1]==v[1] or n[1]==v[2] or n[1]==v[3] or n[1]==v[4]:
       new=new
@@ -18,6 +15,5 @@ else:
       new=new
     else:
       new=new[1:]
-print new
 print n+"!"
 print n+",",n,"bo B"+new,"Bonana fanna fo F"+new,"Fee fy mo M"+new +",", n+"!"
