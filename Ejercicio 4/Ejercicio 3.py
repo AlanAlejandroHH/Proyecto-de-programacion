937ce859401ece5d5a36b92509a8c1b64dc77cd8
alejandronava13 <alejandro13nava7@gmail.com>
2016-12-19 21:15:57 -0600

    Add files via upload
    4ta corrección

--- Ejercicio 4/Ejercicio 3.py
+++ Ejercicio 4/Ejercicio 3.py
@@ -3,25 +3,26 @@
 #Solo correr en python, no ipython
 tn=raw_input('Ingrese Su Archivo De Texto(sin extension) ')
 t=tn+".txt"
-def verificacion(xn,x):
+def verificacion(t,tn):
     cumple=0
     try:
-        archivo=open(x)
-        archivo.close()
+        archivo=open(t)
         cumple=0
     except:
         cumple=1
-    while cumple==1:
-        print"Su archivo no se encuentre en el directorio de ejecucion"
-        xn=raw_input('Por favor, ingrese otro nombre ')
-        x=xn+".txt"
-        try:
-            archivo=open(x)
-            archivo.close()
-            cumple=0
-        except:
-            cumple=1
-verificacion(tn,t)
+        while cumple==1:
+            print"Su archivo no se encuentre en el directorio de ejecucion"
+            tn=raw_input('Por favor, ingrese otro nombre ')
+            t=tn+".txt"
+            try:
+                archivo=open(t)
+                cumple=0
+            except:
+                cumple=1
+    return t
+    return tn
+t=verificacion(t,tn)
+tn=verificacion(t,tn)
 pn=raw_input('Ingrese Su Archivo De Salida(sin extension) ')
 p=pn+".txt"
 tex=open(t, "r")
